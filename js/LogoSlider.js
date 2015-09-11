9b12fe7458c669853aebedd1f811abe52671c016
Ishita Biswas <IshitaBiswas@Ishitas-MacBook-Air.local>
2015-09-10 23:42:40 -0700

    Logo slider rolling down-up from logo

--- js/LogoSlider.js
+++ js/LogoSlider.js
@@ -1,76 +1,76 @@
     /*=====Logo=====*/
-    $('.holdingboxA').hover(function(){
-        $('.rightboxA').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightboxA').animate({width: '0'}, 500)
+    $('.leftboxA').mouseenter(function(){
+        $('.rightboxA').css({top: '-40px',position:'relative',width: '0', height: '0px', left: '420px' }).animate({top: '75px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightboxA').animate({top: '-40px',position:'relative', width: '0', height: '0px', left: '420px'}, 900)
     });
 	
 	/*=====Logo 0=====*/
-	$('.holdingbox0').hover(function(){
-        $('.rightbox0').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox0').animate({width: '0'}, 500)
+	$('.leftbox0').mouseenter(function(){
+        $('.rightbox0').css({top: '-40px',position:'relative',width: '0', height: '0px', left: '580px' }).animate({top: '75px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox0').animate({top: '-40px',position:'relative', width: '0', height: '0px', left: '580px'}, 900)
     });
 	
 	/*=====Logo 1=====*/
-	$('.holdingbox1').hover(function(){
-        $('.rightbox1').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox1').animate({width: '0'}, 500)
+	$('.leftbox1').mouseenter(function(){
+        $('.rightbox1').css({top: '-40px',position:'relative',width: '0', height: '0px', left: '720px' }).animate({top: '75px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox1').animate({top: '-40px',position:'relative', width: '0', height: '0px', left: '720px'}, 900)
     });
 	
 	/*=====Logo 2=====*/
-	$('.holdingbox2').hover(function(){
-        $('.rightbox2').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox2').animate({width: '0'}, 500)
+	$('.leftbox2').mouseenter(function(){
+        $('.rightbox2').css({top: '180px',position:'relative',width: '0', height: '0px', left: '260px' }).animate({top: '400px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox2').animate({top: '180px',position:'relative', width: '0', height: '0px', left: '260px'}, 900)
     });
 	
 	/*=====Logo 3=====*/
-	$('.holdingbox3').hover(function(){
-        $('.rightbox3').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox3').animate({width: '0'}, 500)
+	$('.leftbox3').mouseenter(function(){
+        $('.rightbox3').css({top: '180px',position:'relative',width: '0', height: '0px', left: '420px' }).animate({top: '400px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox3').animate({top: '180px',position:'relative', width: '0', height: '0px', left: '420px'}, 900)
     });
 	
 	/*=====Logo 4=====*/
-	$('.holdingbox4').hover(function(){
-        $('.rightbox4').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox4').animate({width: '0'}, 500)
+	$('.leftbox4').mouseenter(function(){
+        $('.rightbox4').css({top: '180px',position:'relative',width: '0', height: '0px', left: '580px' }).animate({top: '400px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox4').animate({top: '180px',position:'relative', width: '0', height: '0px', left: '580px'}, 900)
     });
 	
 	/*=====Logo 5=====*/
-	$('.holdingbox5').hover(function(){
-        $('.rightbox5').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox5').animate({width: '0'}, 500)
+	$('.leftbox5').mouseenter(function(){
+        $('.rightbox5').css({top: '180px',position:'relative',width: '0', height: '0px', left: '740px' }).animate({top: '400px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox5').animate({top: '180px',position:'relative', width: '0', height: '0px', left: '740px'}, 900)
     });
 	
 	/*=====Logo 6=====*/
-	$('.holdingbox6').hover(function(){
-        $('.rightbox6').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox6').animate({width: '0'}, 500)
+	$('.leftbox6').mouseenter(function(){
+        $('.rightbox6').css({top: '180px',position:'relative',width: '0', height: '0px', left: '900px' }).animate({top: '400px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox6').animate({top: '180px',position:'relative', width: '0', height: '0px', left: '900px'}, 900)
     });
 	
 	/*=====Logo 7=====*/
-	$('.holdingbox7').hover(function(){
-        $('.rightbox7').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox7').animate({width: '0'}, 500)
+	$('.leftbox7').mouseenter(function(){
+        $('.rightbox7').css({top: '420px',position:'relative',width: '0', height: '0px', left: '440px' }).animate({top: '500px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox7').animate({top: '420px',position:'relative', width: '0', height: '0px', left: '440px'}, 900)
     });
 	
 	/*=====Logo 8=====*/
-	$('.holdingbox8').hover(function(){
-        $('.rightbox8').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox8').animate({width: '0'}, 500)
+	$('.leftbox8').mouseenter(function(){
+        $('.rightbox8').css({top: '420px',position:'relative',width: '0', height: '0px', left: '600px' }).animate({top: '500px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox8').animate({top: '420px',position:'relative', width: '0', height: '0px', left: '600px'}, 900)
     });
 	
 	/*=====Logo 9=====*/
-	$('.holdingbox9').hover(function(){
-        $('.rightbox9').animate({width: '1260px', height: '400px'}, 500)
-    }, function(){
-        $('.rightbox9').animate({width: '0'}, 500)
+	$('.leftbox9').mouseenter(function(){
+        $('.rightbox9').css({top: '420px',position:'relative',width: '0', height: '0px', left: '730px' }).animate({top: '500px', width: '1260px', left: '20px', height: '400px'}, 1600)
+    }).mouseleave(function(){
+        $('.rightbox9').animate({top: '420px',position:'relative', width: '0', height: '0px', left: '730px'}, 900)
     });
